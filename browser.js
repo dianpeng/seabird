01da76c81801e9d4102d8e45a707ec0f4b9ea9da
dpeng <dian.peng@verizondigitalmedia.com>
2018-06-22 11:03:04 -0700

    browser

--- browser.js
+++ browser.js
@@ -4,15 +4,13 @@ const printer= require("./lib/printer.js");
 const obj    = require("./lib/object.js" );
 const excep  = require("./lib/exception.js");
 
-module.exports = {
-  parse                   : function(xx)              { return parser(xx);                  },
-  eval                    : function(node,map,dollar) { return eval.Eval(node,map,dollar);  },
-  // rest of the aux functions and objects
-  printer   : printer,
-  object    : obj,
-  exception : excep
-};
-
+window.seabirdParse        = function(xx){ return parser(xx); }
+window.seabirdNewEvaluator = function(map,dollar) {
+  return new eval.Eval(map,dollar);
+}
+window.seabirdPrinter      = printer;
+window.seabirdObject       = obj;
+window.seabirdException    = excep;
 // This file is the entry of the library if user doesn't want to just use it as command line
 // tool but want to use it as a library in Browser. Then user can use whatever pack tools to
 // pack its application an reference it as a third party library
