b01f4ca00ddec67a53fcfcf78a65139da5270c09
dianpeng <dianpengx@gmail.com>
2018-06-22 01:33:50 -0230

    better error report

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function _Main() {
                                "help"    : "show help"
                              });
 
-  {
+  try {
     let args   = parser.Parse ();
     let q = null;
 
@@ -103,6 +103,14 @@ function _Main() {
       console.log(new printer.StrictJSON().Print(r));
     } else {
       console.log(new printer.ExtendedJSON().Print(r));
+    }
+  } catch(e) {
+    if(e instanceof CMDParserError) {
+      console.log(e.msg);
+    } else if(e instanceof sbe.SeabirdException) {
+      console.log(e.msg);
+    } else {
+      console.log("Exception:",e);
     }
   }
 }
