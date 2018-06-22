5aca92508f61763f8117f30afaefb2969e4927fc
dpeng <dian.peng@verizondigitalmedia.com>
2018-06-21 17:17:41 -0700

    still broken

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function _Main() {
                                "help"    : "show help"
                              });
 
-  try {
+  {
     let args   = parser.Parse ();
     let q = null;
 
@@ -103,14 +103,6 @@ function _Main() {
       console.log(new printer.StrictJSON().Print(r));
     } else {
       console.log(new printer.ExtendedJSON().Print(r));
-    }
-  } catch(e) {
-    if(e instanceof CMDParserError) {
-      console.log(e.msg);
-    } else if(e instanceof sbe.SeabirdException) {
-      console.log(e.msg);
-    } else {
-      console.log(util.format("Unknown exception happend:%s",e));
     }
   }
 }
